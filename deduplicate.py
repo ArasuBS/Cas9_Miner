79cc04b14650791f7f85b154ff64731427062db4
ArasuBS <arasuatcalicut@gmail.com>
2026-05-25 13:22:04 +0530

    Update deduplicate.py

--- deduplicate.py
+++ deduplicate.py
@@ -5,14 +5,17 @@ output_file = "cas9_deduplicated.fasta"
 
 seen = set()
 unique_records = []
+total = 0
 
 for record in SeqIO.parse(input_file, "fasta"):
+    total += 1
     seq = str(record.seq)
     if seq not in seen:
         seen.add(seq)
         unique_records.append(record)
 
-print(f"Unique sequences: {len(unique_records)}")
+print(f"Total sequences BEFORE deduplication: {total}")
+print(f"Total sequences AFTER deduplication: {len(unique_records)}")
 
 SeqIO.write(unique_records, output_file, "fasta")
 
